2a8ef68003d582d6681bde541813c8d692ab0590
jiachunxu <leo.jia@oneplus.net>
2016-11-15 18:47:55 +0800

    Added by Oneplus for 3T
    Change-Id: I7e461193a7a28e341c7a66b9eab38b96d54e304f

--- mm-video-v4l2/vidc/venc/Android.mk
+++ mm-video-v4l2/vidc/venc/Android.mk
@@ -37,10 +37,10 @@ libmm-venc-def += -DMAX_RES_1080P_EBI
 endif
 endif
 
-ifeq ($(TARGET_BOARD_PLATFORM),msm8996)
-libmm-venc-def += -D_UBWC_
-libmm-venc-def += -D_VQZIP_
-endif
+#ifeq ($(TARGET_BOARD_PLATFORM),msm8996)
+#libmm-venc-def += -D_UBWC_
+#libmm-venc-def += -D_VQZIP_
+#endif
 
 ifeq ($(call is-board-platform-in-list, $(TARGETS_THAT_USE_FLAG_MSM8226)),true)
 libmm-venc-def += -D_MSM8226_
