2205497ba9a61dbee4842150034dce766fc0940d
vinod <vinod@oneplus.net>
2017-10-19 10:28:20 +0800

    [All][community] add for community
    Plan: as title
    Type: PATCH
    Test: No need to test
    
    Change-Id: Ia624655f4520d0a5261778b9d18b0cf660f17dac
    Signed-off-by: vinod <vinod@oneplus.net>

--- mm-video-v4l2/vidc/venc/Android.mk
+++ mm-video-v4l2/vidc/venc/Android.mk
@@ -52,13 +52,13 @@ ifeq ($(call is-board-platform-in-list, $(TARGETS_THAT_USE_NV21)),true)
 libmm-venc-def += -D_NV21_
 endif
 
-ifeq ($(call is-board-platform-in-list, $(TARGETS_THAT_SUPPORT_VQZIP)),true)
-libmm-venc-def += -D_VQZIP_
-endif
+#ifeq ($(call is-board-platform-in-list, $(TARGETS_THAT_SUPPORT_VQZIP)),true)
+#libmm-venc-def += -D_VQZIP_
+#endif
 
-ifeq ($(call is-board-platform-in-list, $(TARGETS_THAT_SUPPORT_PQ)),true)
-libmm-venc-def += -D_PQ_
-endif
+#ifeq ($(call is-board-platform-in-list, $(TARGETS_THAT_SUPPORT_PQ)),true)
+#libmm-venc-def += -D_PQ_
+#endif
 
 ifeq ($(call is-board-platform-in-list, $(TARGETS_THAT_USE_FLAG_MSM8226)),true)
 libmm-venc-def += -D_MSM8226_
